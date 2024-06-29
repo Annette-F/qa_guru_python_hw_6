842bbe1978afe2a651b8973d209b225cc937a037
Anna Fedorova <anna.fedorova.testing@gmail.com>
2024-06-29 18:31:12 +0300

    исправлены 2 и 4 задания

--- test_homework.py
+++ test_homework.py
@@ -33,12 +33,12 @@ def test_dark_theme_by_time_and_user_choice():
 
     is_dark_theme = None
 
-    if dark_theme_enabled_by_user == None:
-        if current_time >= time(hour=22) or current_time <time(hour=6):
+    if dark_theme_enabled_by_user is None:
+        if current_time >= time(hour=22) or current_time < time(hour=6):
             is_dark_theme = True
         else:
             is_dark_theme = False
-    elif dark_theme_enabled_by_user == True:
+    elif dark_theme_enabled_by_user is True:
         is_dark_theme = True
     else:
         is_dark_theme = False
@@ -98,19 +98,16 @@ def test_readable_function():
 
 
 def open_browser(browser_name):
-    actual_result = None
     actual_result = function_with_arg(open_browser, browser_name)
     assert actual_result == "Open Browser [Chrome]"
 
 
 def go_to_companyname_homepage(page_url):
-    actual_result = None
     actual_result = function_with_arg(go_to_companyname_homepage, page_url)
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
 
 
 def find_registration_button_on_login_page(page_url, button_text):
-    actual_result = None
     actual_result = function_with_arg(find_registration_button_on_login_page, page_url, button_text)
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
 
